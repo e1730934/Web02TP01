7dcf1066d50d7d2d30b6bb36c3cdc469e6fbb455
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-09-18 17:58:44 -0400

    Initial commit

---
+++ app/src/main/java/com/web02_e1730934/tp01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.web02_e1730934.tp01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
